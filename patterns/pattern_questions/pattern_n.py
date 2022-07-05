260b1e5abcf34d1f37c163419d9d3685047a0df7
chandra <sekharc446@gmail.com>
2022-07-05 22:46:10 +0530

    added 05-07-2022

--- patterns/pattern_questions/pattern_n.py
+++ patterns/pattern_questions/pattern_n.py
@@ -1,3 +1,6 @@
+'''pattern_31
+    1..first consider a wall
+    2.. val=min of distances(left,right,top,down) from the wall'''
 def pattern_1(n):
     i=1
     while i<=n:
@@ -163,6 +166,66 @@ def pattern_12(n):
         print()
         i+=1 
 
+def pattern_13(n):
+    i=1
+    while i<=n:
+        j=1
+        while j<=n-i:
+            print(" ",end=" ")
+            j+=1
+        j=1
+        while j<=2*i-1:
+            if i==1 or i==n:
+                print("*",end=" ")
+            else:
+                if j==1 or j==2*i-1:
+                    print("*",end=" ")
+                else:
+                    print(" ",end=" ")
+            j+=1
+        print()
+        i+=1
+
+
+def pattern_14(n):
+    i=1
+    while i<=n:
+        j=1
+        while j<=i-1:
+            print(" ",end=" ")
+            j+=1
+        j=1
+        while j<=2*n-2*i+1:
+            if i==1 or i==n:
+                print("*",end=" ")
+            else:
+                if j==1 or j==2*n-2*i+1:
+                    print("*",end=" ")
+                else:
+                    print(" ",end=" ")
+            j+=1
+        print()
+        i+=1
+
+def pattern_15(n):
+    i=1
+    while i<=2*n-1:
+        cols=2*n-2*(i%n)-1 if i>n else 2*i-1
+        spaces=i-n if i>n else n-i
+        j=1
+        while j<=spaces:
+            print(" ",end=" ")
+            j+=1
+        j=1
+        while j<=cols:
+            if j==1 or j==cols:
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+            j+=1
+        print()
+        i+=1
+
 def pattern_17(n):
     i=1
     while i<=2*n-1:
@@ -182,6 +245,23 @@ def pattern_17(n):
         print()
         i+=1
 
+
+def pattern_20(n):
+    i=1
+    while i<=n:
+        j=1
+        while j<=n-1:
+            if i==1 or i==n:
+                print("*",end=" ")
+            else:
+                if j==1 or j==n-1:
+                    print("*",end=" ")
+                else:
+                    print(" ",end=" ")
+            j+=1
+        print()
+        i+=1       
+
 def pattern_21(n):
     i=1
     num=1
@@ -193,6 +273,43 @@ def pattern_21(n):
             j+=1
         print()
         i+=1
+
+def pattern_22(n):
+    i=1
+    while i<=n:
+        if i%2!=0:
+            num=1
+        else:
+            num=0
+        j=1
+        while j<=i:
+            print(num,end=" ")
+            num=num^1
+
+            j+=1
+        print()
+        i+=1
+
+def pattern_25(n):
+    i=1 
+    while i<=n:
+        j=1
+        while j<=n-i:
+            print(" ",end=" ")
+            j+=1
+        j=1
+        while j<=n:
+            if i==1 or i==n:
+                print("*",end=" ")
+            else:
+                if j==1 or j==n:
+                    print("*",end=" ")
+                else:
+                    print(" ",end=" ")
+            j+=1
+        print()
+        i+=1
+
 
 def pattern_26(n):
     i=1
@@ -207,15 +324,18 @@ def pattern_26(n):
 def pattern_28(n):
     i=1
     while i<=2*n-1:
-        cols=2*n-i if i>n else i
-        spaces=n-cols
+        cols=2*n-2*(i%n)-1 if i>n else 2*i-1
+        spaces=i-n if i>n else n-i
         j=1
         while j<=spaces:
             print(" ",end=" ")
             j+=1
         j=1
         while j<=cols:
-            print("*",end=" ")
+            if j%2!=0:
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
             j+=1
         print()
         i+=1
@@ -240,4 +360,28 @@ def pattern_30(n):
         print()
         i+=1
 
-pattern_21(5)
+
+
+def pattern_31_1(n):
+    i=1
+    while i<=2*n-1:
+        j=1
+        while j<=2*n-1:
+            dis=(min(min(i,j),min(2*n-i,2*n-j)))
+            print(dis,end=" ")
+            j+=1
+        print()
+        i+=1
+
+def pattern_31_2(n):
+    i=1
+    while i<=2*n-1:
+        j=1
+        while j<=2*n-1:
+            dis=n-(min(min(i,j),min(2*n-i,2*n-j)))+1
+            print(dis,end=" ")
+            j+=1
+        print()
+        i+=1
+
+pattern_31_1(4)
